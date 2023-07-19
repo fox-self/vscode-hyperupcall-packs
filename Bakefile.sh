9f9ac7e7ecceae6f1ef8c35ac940eede43d77ac4
Edwin Kofler <edwin@kofler.dev>
2023-07-19 12:51:16 -0700

    Adjust icon size

--- Bakefile.sh
+++ Bakefile.sh
@@ -10,13 +10,13 @@ task.svg() {
 	local ecosystem_color='#e5dbff'
 	local platform_color='#ffe8cc'
 
-	inkscape -w 512 -h 512 ./assets/package.svg -o ./assets/package.png
+	inkscape -w 512 -h 512 -o ./assets/package-sized.png ./assets/package-sized.svg
 	for type in decoration ecosystem platform; do
 		for dir in ./"$type"-packs/*; do
 			local color_var="${type}_color"
 			local -n color="$color_var"
 			mkdir -p "$dir/assets"
-			convert ./assets/package.png -background "$color" -flatten -alpha off "$dir/assets/icon.png"
+			convert ./assets/package-sized.png -background "$color" -flatten -alpha off "$dir/assets/icon.png"
 		done
 	done
 }
